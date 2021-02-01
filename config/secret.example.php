528a1ba5a0aca8d8b12ba2416d572d20b5e2b257
Sergey Gonimar <sergey.gonimar@gmail.com>
2021-02-01 19:19:53 +0500

    fix secret config

--- config/secret.example.php
+++ config/secret.example.php
@@ -4,4 +4,6 @@ return [
     'robokassa.shopId' => 'yii2-robokassa',
     'robokassa.password1' => '',
     'robokassa.password2' => '',
+    'db.username' => 'postgres',
+    'db.password' => 'postgres',
 ];
